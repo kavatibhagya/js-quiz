ce1c9e851d40282ab7b09cc183c6d703b74b1ada
kavatibhagya <bhagyalakshmi.kavati@gmail.com>
2020-02-02 17:03:52 -0800

    adding javascript to the quiz

--- quiz.js
+++ quiz.js
@@ -1,10 +1,62 @@
+const startbutton=document.getElementById('start-btn')
+const nextbutton=document.getElementById('next-btn')
+const questioncontainerElement=document.getElementById('question-container') 
+const questionelement = document.getElementById('question')
+const answerbuttonselement = document.getElementById('answer-buttons')
+  
+
+let shuffledquestions, currentquestionindex
+
+startbutton.addEventListener('click',startgame)
+
 function startgame(){
+    console.log('started')
+    startbutton.classList.add('hide')
+    shuffledquestions = questions.sort(() => Math.random - .5)
+    currentquestionindex = 0
+    questioncontainerElement.classList.remove('hide')
+    setnextquestion()
+
 
 }
-function setnextquetsion(){
+function setnextquestion(){
+    resetstate()
+    showquestion(shuffledquestions[currentquestionindex])
 
+
+}
+function showquestion(question)
+{
+   questionelement.innerText = question.question
+   question.answers.forEach(answer =>{
+       const button = document.createElement('button')
+       button.innerText = answer.text
+       button.classList.add('btn')
+       if(answer.correct){
+           button.dataset.correct =  answer.correct
+       }
+       button.addEventListener('click' ,setanswer)
+       answerbuttonselement.appendChild(button)
+   })
+}
+function resetstate()
+{
+    nextbutton.classList.add('hide')
+    while(answerbuttonselement.firstChild)
+    {
+        answerbuttonselement.removeChild(answerbuttonselement.firstChild)
+    }
 }
 function setanswer(){
 
 }
+const  questions =[
+    {
+        question: 'what is 2+2 ? ',
+        answers: [
+            {text: '4', correct: true},
+            {text: '22', correct: false}
+        ]
+    }
+]
 
